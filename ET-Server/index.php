108c4924c4f50cf0ec858006cbaf8bfb9a96c9dc
Eric Kincl <erick@impactsf.com>
2016-02-23 22:21:46 -0800

    Renamed phoneHome.php to index.php

---
+++ ET-Server/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * 
+ * Client requests file from this server.
+ * Server logs data, including a few extra GET/POST fields
+ * Server responds with .sh shell file.
+ * Client executes .sh file  (Default is just `/dev/null`)
+ * 
+ */
+
+require_once 'lib/global.php';
+
