f8e664a755a200bb4e8be6a9a0d61721a0053423
Eric Kincl <erick@impactsf.com>
2016-03-23 18:55:26 -0700

    Updated default configurations.  Added a few more config options

--- ET-Server/config/config.php
+++ ET-Server/config/config.php
@@ -1,26 +1,29 @@
 <?php
 /*
- * This is where all globa hard config data should be stored
+ * This is where all global hard config data should be stored
  * Database connection info, password, salt, etc...
  */
 
 
-$cfg['log']['debug'] = "/log/debug.html";		// Location of Debug Log.
 
+$cfg['log']['UACheck'] = TRUE;					// Check for proper UA?  FALSE enables logging all requests.
+
+
+$cfg['log']['debug'] = "/log/debug.html";		// Location of Debug Log.
 
 // Where are libraries (classes, interfaces, abstracts, traits) stored?
 $cfg['lib']['root'] 		= '/lib';					// This is the root of where class libraries are stored
-$cfg['lib']['abstract']		= '/';						// This is the sibdirectory which has abstract classes
-$cfg['lib']['class'] 		= '/';						// This is the sibdirectory which has classes
-$cfg['lib']['interface'] 	= '/';						// This is the sibdirectory which has interfaces
-$cfg['lib']['trait']		= '/';						// This is the sibdirectory which has traits
+$cfg['lib']['abstract']		= '/';						// This is the subdirectory which has abstract classes
+$cfg['lib']['class'] 		= '/';						// This is the subdirectory which has classes
+$cfg['lib']['interface'] 	= '/';						// This is the subdirectory which has interfaces
+$cfg['lib']['trait']		= '/';						// This is the subdirectory which has traits
 
 // DB config
 $cfg['db']['host'] 			= "localhost";				// This is wrong.
 $cfg['db']['port'] 			= "3306";
-$cfg['db']['user'] 			= "impactET";
+$cfg['db']['user'] 			= "ET";
 $cfg['db']['pass'] 			= "";
-$cfg['db']['database'] 		= "et";
+$cfg['db']['database'] 		= "ET";
 
 // Force $cfg to be a superglobal.
 global $cfg;
--- ET-Server/config/const.php
+++ ET-Server/config/const.php
@@ -1,5 +1,8 @@
 <?php
-define("DEBUG", TRUE);
+
+define("USER_AGENT", "ET_V.1.0");		// User-Agent string.  MUST match the UA-string on the client side.
+
+define("DEBUG", TRUE);  // Set to FALSE for production servers
 
 // API Error code definitions
 define('ERR_NO_RESULT', '10');
